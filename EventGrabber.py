8434595692b7037ef3e2ee07c0ba828345f58312
Cole Smith <css459@nyu.edu>
2017-02-22 14:41:21 -0500

    Added support for grabbing username. WARNING: THE BOT ID IS CURRENTLY SLACK BACKS. IT WILL NEED ITS OWN.

--- EventGrabber.py
+++ EventGrabber.py
@@ -29,12 +29,21 @@ class EventGrabber(object):
         try:
             data = req.json()
 
+            data_iteration = 0
             for key in data["data"]:
                 att = key["attributes"]
                 event = Event.Event(key["id"], "NO NAME", att["title"], att["startDateTime"], att["endDateTime"])
+
+                included_iteration = 0
+                for keyi in data["included"]:
+                    if included_iteration == data_iteration:
+                        event.creator = keyi["attributes"]["name"]
+                    included_iteration += 1
+
                 event.event_url = key["links"]["self"]
                 event.check_event_for_errors()
                 events.append(event)
+                data_iteration += 1
 
         except ValueError:
             print "[ ERR ] Error in grabbing events from Intranet"
